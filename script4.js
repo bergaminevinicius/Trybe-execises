912913598f7bfaac165c763990cd0c8aff77ef9f
Vinicius Bergamine <vinicius_berg@icloud.com>
2024-01-24 19:43:51 -0300

    exercicio 4

--- script4.js
+++ script4.js
@@ -3,3 +3,15 @@ let degreeAngleB = 100;
 let degreeAngleC = 15;
 
 let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+if(allAnglesArePositives){
+    if (sumOfAngles === 180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+}else {
+    console.log('Erro: ângulo inválido!');
+}
